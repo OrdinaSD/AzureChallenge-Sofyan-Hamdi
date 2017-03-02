34cbd223b8833ea7cdd05cdc5ea176fa9ccc3e72
Unknown <sofyan.hamdi@ordina.nl>
2017-03-02 19:20:11 +0000

    First commit

---
+++ SofyanHamdi-NetworkApp/SofyanHamdi-NetworkApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SofyanHamdi_NetworkApp.Startup))]
+namespace SofyanHamdi_NetworkApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
